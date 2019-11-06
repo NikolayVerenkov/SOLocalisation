5ddf44604ddc880dddf370e61fa5530177f79091
Nikolay Verenkov <nikolay.verenkov@gmail.com>
2019-11-06 17:32:05 +0100

    Initial classes created

---
+++ SOLocalisationManager.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+
+public enum Language { en, ru }
+
+namespace SOLocalisation
+{
+    public class SOLocalisationManager : MonoBehaviour
+    {
+        private static SOLocalisationManager _instance;
+        private static SOLocalisationManager Instance => _instance;
+
+        private void Awake()
+        {
+            if (_instance != null && _instance != this)
+                Destroy(gameObject);
+            else
+                _instance = this;
+        }
+
+        [field: SerializeField,
+                ReadOnly,
+                Tooltip("Current language")]
+        private Language currentLanguage = Language.en;
+        public Language CurrentLanguage => currentLanguage;
+
+        [field: SerializeField,
+                AssetList(AutoPopulate = true),
+                Tooltip("All text localisation scriptable objects")]
+        private SOLocalisationTextAsset[] localisationTextAssets;
+
+        /// <summary>
+        /// Add all currently available language keys to all registered localisation text scriptable objects
+        /// </summary>
+        [Title("Controls")]
+        [Button]
+        private void AddAllLanguageKeys()
+        {
+            foreach (SOLocalisationTextAsset localisationAsset in localisationTextAssets)
+            {
+                localisationAsset.AddAllLanguageKeys();
+            }
+        }
+
+        /// <summary>
+        /// Try to change the current language and if successfull raise a language change event
+        /// </summary>
+        /// <param name="language">Language to set</param>
+        /// <returns>true if the language was different or false if it was already set to this value</returns>
+        [Button]
+        public bool ChangeLanguage(Language language = Language.en)
+        {
+            if (currentLanguage != language)
+            {
+                currentLanguage = language;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        [ButtonGroup("Export and Import")]
+        private void Export()
+        {
+            var sheet = new ES3Spreadsheet();
+            // Add data to cells in the spreadsheet.
+            for (int col = 0; col <= Enum.GetNames(typeof(Language)).Length; col++)
+                for (int row = 0; row <= localisationTextAssets.Length; row++)
+                    if (col == 0 && row == 0) //Top left corner
+                        sheet.SetCell<string>(col, row, "Localisation");
+                    else if (row == 0) //Headers
+                        sheet.SetCell<string>(col, row, GetLanguageStringByIndex(col - 1));
+                    else if (col == 0) //Keys
+                        sheet.SetCell<string>(col, row, localisationTextAssets[row - 1].name);
+                    else //Translations
+                        sheet.SetCell<string>(col, row, localisationTextAssets[row - 1].text[GetLanguageByIndex(col - 1)]);
+            sheet.Save("localisation.csv");
+        }
+
+        [ButtonGroup("Export and Import")]
+        private void Import()
+        {
+
+        }
+
+        [Button]
+        private void TestCSV()
+        {
+            var sheet = new ES3Spreadsheet();
+            // Add data to cells in the spreadsheet.
+            for (int col = 0; col < 10; col++)
+                for (int row = 0; row < 8; row++)
+                    sheet.SetCell<string>(col, row, "someData");
+            sheet.Save("mySheet.csv");
+        }
+
+        private String GetLanguageStringByIndex(int index)
+        {
+            if (Enum.IsDefined(typeof(Language), index))
+                return ((Language)index).ToString();
+            else
+                return "Invalid Value";
+        }
+
+        private Language GetLanguageByIndex(int index)
+        {
+            if (Enum.IsDefined(typeof(Language), index))
+                return ((Language)index);
+            else
+                return Language.en;
+        }
+    }
+}
---
+++ SOLocalisationTextAsset.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+namespace SOLocalisation
+{
+    [CreateAssetMenu(fileName = "LocalisationText", menuName = "Localisation Text")]
+    public class SOLocalisationTextAsset : SerializedScriptableObject
+    {
+        public Dictionary<Language, string> text = new Dictionary<Language, string>
+        {
+            { Language.en, "" },
+            { Language.ru, "" }
+        };
+
+        [Title("Controls")]
+        private void AddLanguage(Language language, string translation = "")
+        {
+            if (!text.ContainsKey(language))
+                text.Add(language, translation);
+        }
+
+        [Button]
+        public void AddAllLanguageKeys()
+        {
+            foreach (Language language in Enum.GetValues(typeof(Language)))
+            {
+                AddLanguage(language);
+            }
+        }
+
+        /// <summary>
+        /// Returns currently selected language
+        /// </summary>
+        /// <returns>Current language</returns>
+        private Language GetCurrentLanguage()
+        {
+            return Language.en; //TODO - fix this
+            //return LocalisationManager.CurrentLanguage;
+        }
+
+        /// <summary>
+        /// Returns text value for the current language
+        /// </summary>
+        /// <returns>Text in current language</returns>
+        public string Get()
+        {
+            return text[GetCurrentLanguage()];
+        }
+    }
+}
