1fdf064f3aaf191c0501635d48992c5388ca6952
Nikolay Verenkov <nikolay.verenkov@gmail.com>
2019-11-25 15:12:58 +0100

    Dropdown selector controller

---
+++ SOLocalisationSelector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SOLocalisation
+{
+    [RequireComponent(typeof(TMP_Dropdown))]
+    public class SOLocalisationSelector : MonoBehaviour
+    {
+        private TMP_Dropdown dropdown;
+
+        private void Start()
+        {
+            //Cache dropdown UI component
+            dropdown = GetComponent<TMP_Dropdown>();
+
+            List<string> languages = new List<string>(Enum.GetNames(typeof(Language)));
+
+            dropdown.AddOptions(languages);
+        }
+
+        public void ChangeLanguageWithIndex(int languageIndex = 0)
+        {
+            SOLocalisationManager.Instance.ChangeLanguageWithIndex(languageIndex);
+        }
+
+    }
+}
+
